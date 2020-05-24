2d9faf489bd48c685f5d19a3a3115d0a271ebf5b
Shimizu-Mitsuharu <mitsuharu.shimizu@gmail.com>
2020-05-24 21:03:48 +0900

    [Update]

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
-var hello = 'Hello World';
-alert(hello);
+var int1 = 1;
+var int2 = -10;
+var float1 = 3.14;
+var str1 = 'JavaScriptを覚えよう';
+alert(int1);
+alert(int2);
+alert(float1);
+alert(str1);
